ba1009ba2d5f1d70db2f12a97f839a6a1c6ca88b
Jacob Luckie <jacobluckie@Jacobs-MacBook-Pro.local>
2018-07-01 13:42:33 +1200

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -16,14 +16,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('new user connected');
 
-    socket.emit('newMessage', {
-        "from": "bobby",
-        "text": "How may I help you?",
-        "createdAt": 123412
-    });
-
-    socket.on('createMessage', (newMessage) => {
-        console.log('newMessage', newMessage);
+    socket.on('createMessage', (message) => {
+        console.log('newMessage', message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
     });
 
     socket.on('disconnect', () => {
